d09d02bb4837d98de7f3671648a2a6b7c11a7ebf
Varma Cephas <varmac231@gmail.com>
2026-01-06 09:32:33 +0200

    fix: remove nestjs logger improper implementation

--- apps/api/src/file-upload/file-upload.controller.ts
+++ apps/api/src/file-upload/file-upload.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Body, Post, Logger } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
 import { ReceiveFileMetadataDto, ReceiveFileMetadataWithPresignUrlDto } from '@repo/api';
 import { FileUploadService } from './file-upload.service';
 
 @Controller('file-upload')
 export class FileUploadController {
-  constructor(public fileUploadService: FileUploadService, private readonly logger: Logger) {}
+  constructor(public fileUploadService: FileUploadService) {}
   @Post('metadata')
   async receiveFilesMetaData(@Body() filesMetaData: ReceiveFileMetadataDto) {
     const filesWithPresignUrl: ReceiveFileMetadataWithPresignUrlDto = await this.fileUploadService.addPresignUrlToFilesMetadata(filesMetaData)
-    this.logger.log(filesWithPresignUrl)
+    console.info(filesWithPresignUrl)
     return filesWithPresignUrl
   }
 }
--- packages/jest-config/src/entry.ts
+++ packages/jest-config/src/entry.ts
@@ -1,3 +1,2 @@
 export * from './base';
 export * from './nest';
-export * from './next';
